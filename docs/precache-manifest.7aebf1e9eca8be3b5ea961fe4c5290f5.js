7838695ae629aa7eedb90cc06bf58bd386419fd5
Aaliya Sayed <aaliyasayed@Aaliyas-MacBook-Pro.local>
2018-11-26 01:28:55 +0530

    docs file to publish github page

---
+++ docs/precache-manifest.7aebf1e9eca8be3b5ea961fe4c5290f5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdef68e805bb7215f02aec6b014afa1e",
+    "url": "/static/media/pune.bdef68e8.jpg"
+  },
+  {
+    "revision": "644be877abe0c6cd3a1d",
+    "url": "/static/css/main.c5a43fdc.chunk.css"
+  },
+  {
+    "revision": "3daafd18d43a53513d13b4bfdf05442f",
+    "url": "/static/media/home_page_banner.3daafd18.jpeg"
+  },
+  {
+    "revision": "6f58a7398b18f7e9428c",
+    "url": "/static/js/1.6f58a739.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ca04ad13ab69ddeb3d6dbe2dce8edb5",
+    "url": "/static/media/bangalore.0ca04ad1.jpg"
+  },
+  {
+    "revision": "38c22997cbfddfbe1c9906ee4a86fa37",
+    "url": "/static/media/delhi.38c22997.jpg"
+  },
+  {
+    "revision": "08e8e1f0d780bd806719cb78e27d12a6",
+    "url": "/static/media/goa.08e8e1f0.jpg"
+  },
+  {
+    "revision": "d92a1e85ffb66d7d0f770851afdc6294",
+    "url": "/static/media/kolkata.d92a1e85.jpg"
+  },
+  {
+    "revision": "b007b10c7ae21b658e341137bd6d90bf",
+    "url": "/static/media/mangalore.b007b10c.jpg"
+  },
+  {
+    "revision": "8e2c82980db2d611fe869dc2de21dd75",
+    "url": "/static/media/mumbai.8e2c8298.jpg"
+  },
+  {
+    "revision": "644be877abe0c6cd3a1d",
+    "url": "/static/js/main.644be877.chunk.js"
+  },
+  {
+    "revision": "6a7e29a5e7c8f4cbaf5edd3f3a93abee",
+    "url": "/static/media/hyderabad.6a7e29a5.jpg"
+  },
+  {
+    "revision": "e4d191e52a255cc857403ec86e6837b8",
+    "url": "/static/media/no_image_small.e4d191e5.jpg"
+  },
+  {
+    "revision": "55f81ecf1bceae7646e237351c5fd9d1",
+    "url": "/static/media/no_image.55f81ecf.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "854ef90c198ed440b215ad9f0db5eb6c",
+    "url": "/static/media/loader.854ef90c.svg"
+  },
+  {
+    "revision": "6f58a7398b18f7e9428c",
+    "url": "/static/css/1.f7f40852.chunk.css"
+  },
+  {
+    "revision": "d9c3771eecabf175ded5b8a79a8f92c2",
+    "url": "/index.html"
+  }
+];
